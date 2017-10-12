45bca53b456d1985d5d8f7f3d948db51b3b42b66
Heather DeMarco <hdemarco@msudenver.edu>
2017-10-11 18:58:34 -0600

    attempting a different way

--- src/Proj1.java
+++ src/Proj1.java
@@ -10,46 +10,51 @@ public class Proj1 {
         System.out.print("enter a value for n: ");
         int n = new Scanner(System.in).nextInt();
         int i = 0;
-        String ones = "1";
         String l = "";
         int o = n;
         String p = "";
+        String m;
+
 
         nums.add(Integer.toString(n));
 
-        int m = doubles(n, nums, l, i);
 
         for(int d = 1; d <n; d++) {
             o = o-1;
             p = p + " + 1";
 
             nums.add(o + p);
+        }
 
+
+        if(n%2 == 0) {
+            m = doubles(n, nums, l, i);
+            nums.add(m);
+        }
+        else{
+            m = doubles(n-1, nums, l, i);
+            nums.add(m + " + 1");
         }
 
         System.out.println(nums);
     }
 
 
-    public static int doubles(int n, Set nums, String l, int i){
+    public static String doubles(int n, Set nums, String l, int i){
 
+        for(int j = 0; j<i ; j++) {
+            l = Integer.toString(n / 2) + " + " + Integer.toString(n / 2);
 
-        if (n%2 == 0) {
-
-                l = Integer.toString(n / 2) + " + " + Integer.toString(n / 2);
-
-                for(int k = 0; k < i; k++) {
-                    l = l + " + " + l;
-                }
-
-                nums.add(l);
-                i++;
-
-                doubles(n / 2, nums, l, i);
+            for (int k = 0; k < i; k++) {
+                l = l + " + " + l;
             }
 
-        return n/2;
 
+            i++;
+
+            return l;
+        }
+        return l;
     }
 
 }
